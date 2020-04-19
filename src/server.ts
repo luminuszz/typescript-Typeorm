5cb84ed0dfe5ae0aba9dc02a3f1365ecff3cbdf3
luminuszz <davibens4@hotmail.com>
2020-04-19 16:29:05 -0300

    fix

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 import 'reflect-metadata';
 import express from 'express';
 
+import uploadConfig from './config/upload.config';
 import routes from './routes/index';
 
 import './database';
@@ -9,6 +10,7 @@ import './database';
 const port = 3333;
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(port, () => {
